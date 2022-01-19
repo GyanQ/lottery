2ee9377e0846c12aa17b172d0fcc26512a1d7cab
Gyan <827792110@qq.com>
2022-01-19 15:38:16 +0800

    project init

---
+++ src/main/java/com/vietnam/lottery/LotteryMain9797.java
@@ -0,0 +1,11 @@
+package com.vietnam.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryMain9797 {
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryMain9797.class, args);
+    }
+}
