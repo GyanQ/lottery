aeb856834fae37b3dbb6ef71a24a6dbca288c2f2
Gyan <827792110@qq.com>
2022-01-24 10:10:00 +0800

    update project

--- src/main/java/com/vietnam/lottery/common/config/SwaggerConfig.java
+++ src/main/java/com/vietnam/lottery/common/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
 package com.vietnam.lottery.common.config;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -27,43 +27,33 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public Docket createWeb() {
+    public Docket createBack() {
         return new Docket(DocumentationType.SWAGGER_2)
                 // 是否启用Swagger
                 .enable(true)
                 // 用来创建该API的基本信息，展示在文档的页面中（自定义展示的信息）
                 .apiInfo(apiInfo())
                 // 设置哪些接口暴露给Swagger展示
-                .groupName("web管理后台")
-                .globalOperationParameters(getGlobalOperationParameters())
-                .select()
+                .groupName("web管理后台").globalOperationParameters(getGlobalOperationParameters()).select()
                 // 扫描所有有注解的api，用这种方式更灵活
-                .apis(RequestHandlerSelectors.withMethodAnnotation(RequestMapping.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 // 扫描指定包中的swagger注解
-                .apis(RequestHandlerSelectors.basePackage("com.vietnam.lottery.content.web"))
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/");
+                .apis(RequestHandlerSelectors.basePackage("com.vietnam.lottery.content.back")).paths(PathSelectors.any()).build().pathMapping("/");
     }
 
     @Bean
-    public Docket createApp() {
+    public Docket createFront() {
         return new Docket(DocumentationType.SWAGGER_2)
                 // 是否启用Swagger
                 .enable(true)
                 // 用来创建该API的基本信息，展示在文档的页面中（自定义展示的信息）
                 .apiInfo(apiInfo())
                 // 设置哪些接口暴露给Swagger展示
-                .groupName("**")
-                .globalOperationParameters(getGlobalOperationParameters())
-                .select()
+                .groupName("web抽奖").globalOperationParameters(getGlobalOperationParameters()).select()
                 // 扫描所有有注解的api，用这种方式更灵活
-                .apis(RequestHandlerSelectors.withMethodAnnotation(RequestMapping.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 // 扫描指定包中的swagger注解
-                .apis(RequestHandlerSelectors.basePackage("com.vietnam.lottery.content.app"))
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/");
+                .apis(RequestHandlerSelectors.basePackage("com.vietnam.lottery.content.front")).paths(PathSelectors.any()).build().pathMapping("/");
     }
 
     /**
@@ -78,8 +68,7 @@ public class SwaggerConfig {
                 // 作者信息
                 .contact("Gyan")
                 // 版本
-                .version("1.0.0")
-                .build();
+                .version("1.0.0").build();
     }
 
     /**
