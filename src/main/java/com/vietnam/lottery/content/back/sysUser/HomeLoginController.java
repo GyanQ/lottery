52e9239654f6f40114fa64131d3143d0589a016d
Gyan <827792110@qq.com>
2022-02-09 12:08:33 +0800

    修改密码

--- src/main/java/com/vietnam/lottery/content/back/sysUser/HomeLoginController.java
+++ src/main/java/com/vietnam/lottery/content/back/sysUser/HomeLoginController.java
@@ -1,7 +1,6 @@
 package com.vietnam.lottery.content.back.sysUser;
 
 import com.vietnam.lottery.business.sysUser.request.LoginRequest;
-import com.vietnam.lottery.business.sysUser.request.UpdatePawRequest;
 import com.vietnam.lottery.business.sysUser.service.SysUserService;
 import com.vietnam.lottery.common.config.JwtUtil;
 import com.vietnam.lottery.common.utils.ResultModel;
@@ -15,14 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
 @Api(tags = "首页")
-@RequestMapping("/sys/user")
+@RequestMapping("/sys/home")
 public class HomeLoginController {
     @Autowired
     private SysUserService sysUserService;
@@ -36,17 +34,5 @@ public class HomeLoginController {
         Map<String, Object> map = sysUserService.login(request);
         httpServletResponse.setHeader(JwtUtil.getHeader(), map.get("token").toString());
         return ResultUtil.success("登录成功！");
-    }
-
-    @PostMapping("/updatePaw")
-    @ApiOperation("修改密码")
-    public ResultModel updatePaw(@Valid @RequestBody UpdatePawRequest request, BindingResult bindingResult, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            return ResultUtil.failure(bindingResult.getFieldError().getDefaultMessage());
-        }
-        String token = httpServletRequest.getHeader(JwtUtil.getHeader());
-        String userId = JwtUtil.parseToken(token);
-        request.setCreateBy(Long.valueOf(userId));
-        return ResultUtil.success(sysUserService.updatePaw(request));
     }
 }
--- src/main/java/com/vietnam/lottery/content/back/sysUser/SysUserController.java
+++ src/main/java/com/vietnam/lottery/content/back/sysUser/SysUserController.java
@@ -1,10 +1,21 @@
 package com.vietnam.lottery.content.back.sysUser;
 
+import com.vietnam.lottery.business.sysUser.request.UpdatePawRequest;
 import com.vietnam.lottery.business.sysUser.service.SysUserService;
+import com.vietnam.lottery.common.config.JwtUtil;
+import com.vietnam.lottery.common.utils.ResultModel;
+import com.vietnam.lottery.common.utils.ResultUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @Api(tags = "账户管理")
@@ -13,5 +24,27 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
+    @PostMapping("/updatePaw")
+    @ApiOperation("修改密码")
+    public ResultModel updatePaw(@Valid @RequestBody UpdatePawRequest request, BindingResult bindingResult, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            return ResultUtil.failure(bindingResult.getFieldError().getDefaultMessage());
+        }
+        String token = httpServletRequest.getHeader(JwtUtil.getHeader());
+        String userId = JwtUtil.parseToken(token);
+        request.setCreateBy(Long.valueOf(userId));
+        return ResultUtil.success(sysUserService.updatePaw(request));
+    }
 
+    @PostMapping("/createAccount")
+    @ApiOperation("创建账号")
+    public ResultModel createAccount(@Valid @RequestBody UpdatePawRequest request, BindingResult bindingResult, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            return ResultUtil.failure(bindingResult.getFieldError().getDefaultMessage());
+        }
+        String token = httpServletRequest.getHeader(JwtUtil.getHeader());
+        String userId = JwtUtil.parseToken(token);
+        request.setCreateBy(Long.valueOf(userId));
+        return ResultUtil.success(sysUserService.updatePaw(request));
+    }
 }
