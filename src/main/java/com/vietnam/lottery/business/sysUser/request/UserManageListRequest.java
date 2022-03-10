c1d5a08c573053800839d564238073419d92adba
Gyan <yangqg97@gmail.com>
2022-03-10 12:19:36 +0800

    update 用户列表

--- src/main/java/com/vietnam/lottery/business/sysUser/request/UserManageListRequest.java
+++ src/main/java/com/vietnam/lottery/business/sysUser/request/UserManageListRequest.java
@@ -11,14 +11,8 @@ import java.io.Serializable;
 public class UserManageListRequest extends PageRequest implements Serializable {
     private static final long serialVersionUID = -9191474827254230835L;
 
-    @ApiModelProperty(value = "userId")
-    private String userId;
-
-    @ApiModelProperty(value = "手机")
-    private String phone;
-
-    @ApiModelProperty(value = "账号")
-    private String account;
+    @ApiModelProperty(value = "账号、手机、userId")
+    private String keyWord;
 
     @ApiModelProperty(value = "开始时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
@@ -30,4 +24,7 @@ public class UserManageListRequest extends PageRequest implements Serializable {
 
     @ApiModelProperty(value = "排序：1余额升序 2余额降序 3充值升序 4充值降序 5收益升序 6收益降序")
     private String type;
+
+    @ApiModelProperty(value = "删除标志（0代表存在、1代表删除）")
+    private String delFlag;
 }
