83e9cc67f69e7e6e5d4512df46001afcda29e06d
Gyan <yangqg97@gmail.com>
2022-04-08 00:02:58 +0800

    update 充值配置

---
+++ src/main/java/com/vietnam/lottery/content/front/recharge/RechargeWebController.java
@@ -0,0 +1,29 @@
+package com.vietnam.lottery.content.front.recharge;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.vietnam.lottery.business.recharge.request.RechargeListRequest;
+import com.vietnam.lottery.business.recharge.response.RechargeListResponse;
+import com.vietnam.lottery.business.recharge.service.RechargeService;
+import com.vietnam.lottery.common.utils.ResultModel;
+import com.vietnam.lottery.common.utils.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "充值配置")
+@RequestMapping("/web/recharge")
+public class RechargeWebController {
+    @Autowired
+    private RechargeService rechargeService;
+
+    @PostMapping("/list")
+    @ApiOperation("列表")
+    public ResultModel<Page<RechargeListResponse>> add(@RequestBody RechargeListRequest request) {
+        return ResultUtil.success(rechargeService.list(request));
+    }
+}
