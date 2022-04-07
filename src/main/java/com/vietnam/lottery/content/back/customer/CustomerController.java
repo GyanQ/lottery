7c43d5c1089372a49931aa6a9a5134b9dc233e3a
Gyan <yangqg97@gmail.com>
2022-04-08 02:01:21 +0800

    add 上传

---
+++ src/main/java/com/vietnam/lottery/content/back/customer/CustomerController.java
@@ -0,0 +1,45 @@
+package com.vietnam.lottery.content.back.customer;
+
+import com.vietnam.lottery.business.customer.service.CustomerService;
+import com.vietnam.lottery.common.utils.ResultModel;
+import com.vietnam.lottery.common.utils.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@Api(tags = "客服配置")
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/upload")
+    @ApiOperation("上传")
+    public ResultModel add(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        //文件存放地址
+        String filePath = request.getSession().getServletContext().getRealPath("opt/lottery/img");
+        //文件名称
+        String fileName = file.getOriginalFilename();
+        File fileDir = new File(filePath);
+
+        try {
+            file.transferTo(fileDir);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fileName = filePath + "/" + fileName;
+        return ResultUtil.success(fileName);
+    }
+}
