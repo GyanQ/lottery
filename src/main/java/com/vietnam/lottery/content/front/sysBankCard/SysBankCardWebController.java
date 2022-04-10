8986db6e023c340f0c5866ddfa1c22cf187a4afd
Gyan <yangqg97@gmail.com>
2022-04-10 15:53:29 +0800

    update

--- src/main/java/com/vietnam/lottery/content/front/sysBankCard/SysBankCardWebController.java
+++ src/main/java/com/vietnam/lottery/content/front/sysBankCard/SysBankCardWebController.java
@@ -8,7 +8,6 @@ import com.vietnam.lottery.common.utils.ResultModel;
 import com.vietnam.lottery.common.utils.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(tags = "用户银行卡")
+@Api(tags = "银行卡")
 @RequestMapping("/web/back/crad")
 public class SysBankCardWebController {
     @Autowired
