4f70e8f32bc6d681d32246409517685ff7aea1c3
Gyan <yangqg97@gmail.com>
2022-04-17 23:18:46 +0800

    test 主动获取订单

--- src/main/java/com/vietnam/lottery/business/rechargeDetail/service/impl/RechargeDetailServiceImpl.java
+++ src/main/java/com/vietnam/lottery/business/rechargeDetail/service/impl/RechargeDetailServiceImpl.java
@@ -140,11 +140,11 @@ public class RechargeDetailServiceImpl implements RechargeDetailService {
         JSONObject data = json.getJSONObject("data");
         log.info("获取data,{}", data);
         if (data.getInt("ispay") != 1) {
-            return false;
+            return true;
         }
         GrabRedPackets grabRedPackets = grabRedPacketsMapper.selectById(request.getId());
         if (ObjectUtil.isEmpty(grabRedPackets)) {
-            return false;
+            return true;
         }
         SysUserAccount userAccount = new SysUserAccount();
         userAccount.setProductId(request.getId());
