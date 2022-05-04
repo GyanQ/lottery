746f83e8795c847220ddcdb7158e9e7eaff5396b
Gyan <yangqg97@gmail.com>
2022-05-04 13:30:27 +0800

    。。

--- src/main/java/com/vietnam/lottery/business/basicIndicators/request/ProbabilityRequest.java
+++ src/main/java/com/vietnam/lottery/business/basicIndicators/request/ProbabilityRequest.java
@@ -13,11 +13,11 @@ public class ProbabilityRequest implements Serializable {
     private static final long serialVersionUID = 4318011500778794268L;
 
     @ApiModelProperty(value = "开始时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private String beginDate;
 
     @ApiModelProperty(value = "结束时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private String endDate;
 
 }
