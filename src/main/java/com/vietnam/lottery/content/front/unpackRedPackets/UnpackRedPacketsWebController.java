19450aa33008131f0d0548196a2204111dab256d
Gyan <yangqg97@gmail.com>
2022-05-04 10:01:08 +0800

    1

--- src/main/java/com/vietnam/lottery/content/front/unpackRedPackets/UnpackRedPacketsWebController.java
+++ src/main/java/com/vietnam/lottery/content/front/unpackRedPackets/UnpackRedPacketsWebController.java
@@ -1,6 +1,5 @@
 package com.vietnam.lottery.content.front.unpackRedPackets;
 
-import com.vietnam.lottery.business.sysBroadcastConfig.service.SysBroadcastConfigService;
 import com.vietnam.lottery.business.unpackRedPackets.response.BroadcastResponse;
 import com.vietnam.lottery.business.unpackRedPackets.service.UnpackRedPacketsService;
 import com.vietnam.lottery.common.utils.JwtUtil;
@@ -8,7 +7,10 @@ import com.vietnam.lottery.common.utils.ResultModel;
 import com.vietnam.lottery.common.utils.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +32,7 @@ public class UnpackRedPacketsWebController {
 
     @GetMapping("/broadcast")
     @ApiOperation("广播")
-    public ResultModel<BroadcastResponse> broadcast(@PathVariable("flag") Boolean flag) {
+    public ResultModel<BroadcastResponse> broadcast(Boolean flag) {
         return ResultUtil.success(unpackRedPacketsService.broadcast(flag));
     }
 }
