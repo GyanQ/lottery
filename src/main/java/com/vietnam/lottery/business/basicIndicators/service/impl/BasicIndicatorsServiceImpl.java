b6dbb3c79fc38e6e8b00361b95a608abfb51d3d9
Gyan <yangqg97@gmail.com>
2022-06-18 21:18:49 +0800

    1

--- src/main/java/com/vietnam/lottery/business/basicIndicators/service/impl/BasicIndicatorsServiceImpl.java
+++ src/main/java/com/vietnam/lottery/business/basicIndicators/service/impl/BasicIndicatorsServiceImpl.java
@@ -9,7 +9,6 @@ import com.vietnam.lottery.business.basicIndicators.response.KeepListResponse;
 import com.vietnam.lottery.business.basicIndicators.service.BasicIndicatorsService;
 import com.vietnam.lottery.business.unpackRedPackets.mapper.UnpackRedPacketsMapper;
 import com.vietnam.lottery.common.utils.DateUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -17,7 +16,6 @@ import org.springframework.util.CollectionUtils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -56,7 +54,6 @@ public class BasicIndicatorsServiceImpl implements BasicIndicatorsService {
         //结束时间格式化
         DateTimeFormatter formatEnd = DateTimeFormatter.ofPattern("yyyy-MM-dd 23:59:59");
 
-
         String date = DateUtils.getCurrentTimeStr(DateUtils.DATETIME_PATTERN);
         //筛选时间
         LocalDateTime local = DateUtils.parseLocalDateTime(request.getBeginDate(), DateUtils.DATETIME_PATTERN);
@@ -67,31 +64,36 @@ public class BasicIndicatorsServiceImpl implements BasicIndicatorsService {
         List<String> secondCount = unpackRedPacketsMapper.keep(formatBegin.format(minus), formatEnd.format(minus));
         int secondSize = userIds.stream().filter(o -> secondCount.contains(o)).collect(Collectors.toList()).size();
         resp.setSecondStay(secondSize);
-        resp.setSecondPer((secondSize / userIds.size()) * 100);
+        int secondNum = (0 == secondSize) ? 0 : (secondSize / userIds.size()) * 100;
+        resp.setSecondPer(secondNum);
         //3留
         LocalDateTime treeStay = DateUtils.minus(local, 3, ChronoUnit.DAYS);
         List<String> treeCount = unpackRedPacketsMapper.keep(formatBegin.format(treeStay), formatEnd.format(treeStay));
         int treeSize = userIds.stream().filter(o -> treeCount.contains(o)).collect(Collectors.toList()).size();
         resp.setThree(treeSize);
-        resp.setThreePer((treeSize / userIds.size()) * 100);
+        int treeNum = (0 == treeSize) ? 0 : (treeSize / userIds.size()) * 100;
+        resp.setThreePer(treeNum);
         //7留
         LocalDateTime serverStay = DateUtils.minus(local, 7, ChronoUnit.DAYS);
         List<String> serverCount = unpackRedPacketsMapper.keep(formatBegin.format(serverStay), formatEnd.format(serverStay));
         int serverSize = userIds.stream().filter(o -> serverCount.contains(o)).collect(Collectors.toList()).size();
         resp.setSevenStay(serverSize);
-        resp.setSevenStayPer((serverSize / userIds.size()) * 100);
+        int serverNum = (0 == serverSize) ? 0 : (serverSize / userIds.size()) * 100;
+        resp.setSevenStayPer(serverNum);
         //15留
         LocalDateTime fifteenStay = DateUtils.minus(local, 15, ChronoUnit.DAYS);
         List<String> fifteenCount = unpackRedPacketsMapper.keep(formatBegin.format(fifteenStay), formatEnd.format(fifteenStay));
         int fifteenSize = userIds.stream().filter(o -> fifteenCount.contains(o)).collect(Collectors.toList()).size();
         resp.setFifteenStay(fifteenSize);
-        resp.setFifteenStayPer((fifteenSize / userIds.size()) * 100);
+        int fifteenNum = (0 == fifteenSize) ? 0 : (fifteenSize / userIds.size()) * 100;
+        resp.setFifteenStayPer(fifteenNum);
         //30留
         LocalDateTime monthStay = DateUtils.minus(local, 30, ChronoUnit.DAYS);
         List<String> monthCount = unpackRedPacketsMapper.keep(formatBegin.format(monthStay), formatEnd.format(monthStay));
         int monthSize = userIds.stream().filter(o -> monthCount.contains(o)).collect(Collectors.toList()).size();
         resp.setMonthStay(monthSize);
-        resp.setMonthStayPer((monthSize / userIds.size()) * 100);
+        int monthNum = (0 == monthSize) ? 0 : (monthSize / userIds.size()) * 100;
+        resp.setMonthStayPer(monthNum);
         return resp;
     }
 }
